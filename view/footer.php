57968443d3478f2939bd97a18c04a67f462e8daf
Carl Jonar Palado <cjpalado@gmail.com>
2023-12-11 06:35:23 +0800

    Initial Commit

---
+++ view/footer.php
@@ -0,0 +1,9 @@
+	<footer class="main-footer">
+        <div class="pull-right hidden-xs">
+          <b>Version</b> 1.0
+        </div>
+        <strong>Copyright &copy; 2023-2024 <a href="google.com">CAPSTONE</a>.</strong> All rights reserved.
+     </footer>
+      
+  </body>
+</html>
